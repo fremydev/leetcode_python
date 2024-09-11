befed70a4905f6b206977c40aed197925873a535
fremydev <fremy.dev@gmail.com>
2024-09-11 09:19:53 -0300

    adds palindrome_number exercise

---
+++ palindrome_number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        num = x
+        reversed_num = 0
+
+        while x != 0:
+            reversed_num = reversed_num * 10 + x % 10
+            x = x // 10
+
+        return reversed_num == num
